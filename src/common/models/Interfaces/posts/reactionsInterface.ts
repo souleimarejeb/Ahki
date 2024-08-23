3d7eaed013b4c9e2c5f10328be61712f4faedf48
souleimarejeb <80159191+souleimarejeb@users.noreply.github.com>
2024-08-23 09:28:10 +0100

    fix : fixed reaction.services

--- src/common/models/Interfaces/posts/reactionsInterface.ts
+++ src/common/models/Interfaces/posts/reactionsInterface.ts
@@ -1,5 +1,11 @@
+import { IUsers } from "../UserInterface";
+import { IPosts } from "./postInterface";
 
 export interface IReactions {
 
     type: string;
+
+    post: Partial<IPosts>;
+
+    user: Partial<IUsers>;
 }
--- src/modules/post/Services/reaction.service.ts
+++ src/modules/post/Services/reaction.service.ts
@@ -1,34 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReactionsEntity } from 'src/common/models/db/Posts/reactions.entity';
 import { IReactions } from 'src/common/models/Interfaces/posts/reactionsInterface';
+import { UserService } from 'src/modules/users/user.service';
 import { Repository } from 'typeorm/repository/Repository';
+import { PostService } from './post.service';
 
 @Injectable()
 export class ReactionService {
-    constructor(@InjectRepository(ReactionsEntity) private reactiontRepository: Repository<ReactionsEntity>,
+    constructor(
+        @InjectRepository(ReactionsEntity) private reactiontRepository: Repository<ReactionsEntity>,
+        private readonly userService: UserService,
+        private readonly postService: PostService,
     ) { }
-    create(reactionDetails: IReactions) {
-        const newReaction = this.reactiontRepository.create({
-            ...reactionDetails,
 
-        });
-        return this.reactiontRepository.save(newReaction)
+    async create(payload: Partial<IReactions>, postId: string, userId: string) {
+        try {
+
+            const user = await this.userService.findOne(userId);
+            if (!user) throw new HttpException('user  not found.  ', HttpStatus.BAD_REQUEST);
+
+            const post = await this.postService.findOne(postId, "{}");
+            if (!post) throw new HttpException('post  not found.', HttpStatus.BAD_REQUEST);
+
+            const foundReaction = await this.reactiontRepository
+                .createQueryBuilder('reactions')
+                .where('reactions.post = :postId', { postId: postId })
+                .andWhere('reactions.user = :userId', { userId: userId })
+                .getOne();
+
+            if (foundReaction) throw new HttpException(' Cannot post new reaction  ', HttpStatus.BAD_REQUEST);
+
+            const newReaction = this.reactiontRepository.create({
+                ...payload,
+                post: post.data[0],
+                user: user[0],
+            });
+
+            return await this.reactiontRepository.save(newReaction);
+
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    findAll() {
-        return this.reactiontRepository.find();
+    async findAll(postId: string) {
+        try {
+            const foundReactions = await this.reactiontRepository
+                .find({
+                    relations: {
+                        user: true,
+                    },
+                    where: {
+                        post: {
+                            id: postId,
+                        },
+                    },
+                })
+
+            return foundReactions;
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    findOne(id: string) {
-        return this.reactiontRepository.findBy({ id });
-    }
-
-    update(id: string, updatereaction: Partial<IReactions>) {
-        return this.reactiontRepository.update({ id }, { ...updatereaction });
+    update(id: string, payload: Partial<IReactions>) {
+        try {
+            return this.reactiontRepository.update({ id }, { ...payload });
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     remove(id: string) {
-        return this.reactiontRepository.delete({ id });
+        try {
+            return this.reactiontRepository.delete({ id });
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
